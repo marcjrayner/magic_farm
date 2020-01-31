4b69e5dfc1b94a96f1639acd3f634cda43b1386d
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-31 12:28:07 +0000

    initial commit

---
+++ server/src/main/java/com/codeclan/example/magic_farm/MagicFarmApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.magic_farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicFarmApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codeclan/example/magic_farm/MagicFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.magic_farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
