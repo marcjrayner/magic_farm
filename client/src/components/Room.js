e36679f1d8ab0dcb9e3f37d67ad306e902afe316
Richard <fahitch+github@gmail.com>
2020-02-01 18:10:13 +0000

    room renders on grid based on position

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -16,8 +16,8 @@ const Room = (props) => {
     const roomRender = room.cellArray.map( (roomCell, index) => {
         
         const position = [];
-        const x = roomCell[0] + (translateX - 1);
-        const y = roomCell[1] + (translateY - 1);
+        const x = roomCell[0];
+        const y = roomCell[1];
         position.push(x);
         position.push(y);
         const name = ('' + room.roomType + ' ' + room.roomStatus +'-'+position)
@@ -39,7 +39,9 @@ const Room = (props) => {
         gridTemplateRows: 'repeat(' + room.roomMaxHeight + ', 20px)',
         gridTemplateColumns: 'repeat(' + room.roomMaxWidth + ', 20px)',
         gridRowStart: translateX,
-        gridColumnStart: translateY
+        gridColumnStart: translateY,
+        gridRowEnd: translateX + room.roomMaxWidth,
+        gridColumnEnd: translateY + room.roomMaxHeight
     };
 
     return(
