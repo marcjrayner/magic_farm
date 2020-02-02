12f4d24483539b1633ffecba3fe8f44843b79c15
Richard <fahitch+github@gmail.com>
2020-02-02 10:32:02 +0000

    cards now selectable

--- client/src/cards/ChoiceCard.js
+++ client/src/cards/ChoiceCard.js
@@ -21,21 +21,30 @@ const ChoiceCard = (props) => {
     let cardStyle = {};
 
     function selectCard(e){
-        console.log(e.target)
+        if (selectedCard === false){
+            console.log(e.target)
 
         props.clickMethod({objectType: props.choiceData.objectType, 
-            objectData: props.choiceData.objectData})
+            objectData: props.choiceData.objectData});
+            setSelectedCard(true);
+
+        } else {
+            setSelectedCard(false);
+
+        }
     }
 
     return (
         <article className={"card " + (selectedCard? 'selected-card ' : '')}
             onClick={selectCard}
-            style={cardStyle}
+            // style={cardStyle}
             // onClick={handleChange}
             // onHover={handleHover}
              >
-
-                <Room room={card}
+                <h2>{props.choiceData.objectType}</h2> 
+                <Room 
+                className='card-content'
+                room={card}
                 position={props.choiceData.objectPosition}
                 />
 
