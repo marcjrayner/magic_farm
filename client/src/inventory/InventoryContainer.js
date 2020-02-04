13bf225242a20d61c1c7bae28ab277382a2dc396
Richard <fahitch+github@gmail.com>
2020-02-04 10:40:57 +0000

    passed data to component

--- client/src/inventory/InventoryContainer.js
+++ client/src/inventory/InventoryContainer.js
@@ -1,7 +1,52 @@
-import React, {Component} from "react";
+import React from "react";
 
 
-class InventoryContainer extends Component {
+const InventoryContainer = (props) => {
+
+    // This is what data looks like
+    // userInventoryData: {
+    //     coins: 100;
+    //     numberOfRooms: 0;
+    //     areaCovered: 0;
+    //     animals: 0;
+    //     level: 1;
+    //     score: 0;
+    // }
+
+    // gameBoard: {
+    //     width: 20,
+    //     height: 10,
+    //     cellSize: 20
+    // },
+    
+    const gameBoard = props.gameBoard;
+    const inventory = props.userInventoryData;
+    const area = ( inventory.areaCovered ) / ( gameBoard.width * gameBoard.height) * 100;
+   
+        return (
+            <section id="inventory-container" >
+                
+                <aside id="coins-display">
+                    <p class="ui-coins" >{inventory.coins}</p>
+                </aside>
+
+                <aside id="playing-data" >
+                    <p class="user-data ui-rooms">
+                        Rooms: {inventory.numberOfRooms} | {area}%
+                    </p>
+                
+                    <p class="user-data ui-animals">
+                        Animals: {inventory.animals}
+                    </p>
+
+                </aside>
+
+                <aside id="level-display">
+                Level {this.props.userInventoryData.level}
+                </aside>
+            </section>
+        )
+    }
 
 }
 
