aa5b5b9c8706006ea6d7797e544b949bb2e3a1b1
Aresky Berkane <areskyberkane@gmail.com>
2020-02-05 12:29:24 +0000

    new database\

--- server/src/main/java/com/codeclan/example/magic_farm/repositories/RoomTypeRepository.java
+++ server/src/main/java/com/codeclan/example/magic_farm/repositories/RoomTypeRepository.java
@@ -1,9 +1,9 @@
 package com.codeclan.example.magic_farm.repositories;
 
-        import com.codeclan.example.magic_farm.models.Room;
+        import com.codeclan.example.magic_farm.models.RoomType;
         import org.springframework.data.jpa.repository.JpaRepository;
         import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface RoomRepository extends JpaRepository<Room,Long> {
+public interface RoomTypeRepository extends JpaRepository<RoomType,Long> {
 }
